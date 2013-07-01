bd5663d3a4b2971bd21338b5ebcc2053be7eb181
sean <sean@sean-ThinkPad-W520.(none)>
2013-07-01 01:28:04 -0400

    Fixed full contraction initialization

--- motorControl.cpp
+++ motorControl.cpp
@@ -39,14 +39,16 @@ void MotorControl::_init(int extendPinA, int extendPinB, int contractPinA, int c
   pinMode(encoderPin, INPUT);
   
   _state = STOP;
-  _edgeCount = MAX_EDGE_COUNT;
+  _edgeCount = 0;
   _prevEncoderVal = 0;
   _noChangeCount = 0;
 
-  // Make motor completely contracted, while initializing edgeCount to 0.
-  while(contract()) {
-    _edgeCount--;
-  }
+  // Make motor completely contracted. 16000 is chosen for the delay because
+  // at .5 in per second, it will take 16 seconds to contract the potential full 
+  // 8 inches.
+  _contract();
+  delay(16000);
+  stop();
 }
 
 void MotorControl::updateEncoder() {
